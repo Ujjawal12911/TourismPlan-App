abc3a2213f486ef3ffc5db25e7ee6cfc29c0ec0f
Ujjawal12911 <158555141+Ujjawal12911@users.noreply.github.com>
2024-08-14 22:55:07 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import data from './data.js';
+import Tours from './Component/Tours';
+
+const App = () => {
+ 
+
+  
+
+  return  <div>App</div>
+
+};
+
+export default App;
